8550cb5dfab576eb0358df642bac7cd96ac36cd8
sarahob96 <sarahobrien15@gmail.com>
2022-07-09 15:09:52 +0000

    add hero image to the home page and edit settings.py to link the image

--- django_resetbali/settings.py
+++ django_resetbali/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
