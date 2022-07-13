9e2379e8cb362270d93baa30348910486c28e7d3
sarahob96 <sarahobrien15@gmail.com>
2022-07-13 22:00:51 +0000

    set up the views and urls.py for the accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
- 
+    path('account_login', views.account_login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
-# Create your views here.
+
+def account_login(request):
+    return render(request, 'accounts/accounts.html', {})
