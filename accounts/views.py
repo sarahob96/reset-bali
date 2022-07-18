9184e890a23faed0f445f13fc3a6d4cf79f53992
sarahob96 <sarahobrien15@gmail.com>
2022-07-18 14:03:45 +0000

    add a messages block to showcase any login errors

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,18 @@ def new_account(request):
     return render(request, 'accounts/registration.html', context)
 
 def login_form(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('register')
+        else:
+            messages.success(request, ("Error with logging in, please try again"))
+            return redirect('login')
+
     context = {}
     return render(request, 'accounts/login.html', context)
 
