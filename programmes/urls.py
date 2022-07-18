fd17f8a0f1138588c05c92d2c5793319b6ba6d2c
sarahob96 <sarahobrien15@gmail.com>
2022-07-18 19:02:09 +0000

    add images to the locations page within the programmes app and resize

--- programmes/urls.py
+++ programmes/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
   
-
     path('locations', views.locations, name="locations"),
     path('rewind', views.locations, name="rewind"),
     path('restart', views.locations, name="restart"),
     path('renew', views.locations, name="renew"),
-    
+
 ]
