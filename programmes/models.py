38ab5397e83a188d8fa1381aa8fa93f94ea62d06
sarahob96 <sarahobrien15@gmail.com>
2022-07-23 10:08:25 +0000

    render logged in users 'username' to the homepage

--- programmes/models.py
+++ programmes/models.py
@@ -11,4 +11,7 @@ class rewind(models.Model):
     date = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     phone = models.IntegerField()
-    
+
+
+def __str__(self):
+    return self.title
