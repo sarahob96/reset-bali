948c4138f07a09968c87f03a6d9b8ff78be197c5
sarahob96 <sarahobrien15@gmail.com>
2022-07-25 22:28:11 +0000

    add crispy tag to all forms

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django import forms
 
 # Create your models here.
 
@@ -9,9 +9,10 @@ class Review(models.Model):
     """
 
     STARS = ((1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"))
+    PROGRAMMES = (("Rewind", "Rewind"), ("Renew", "Renew"), ("Restart", "Restart"))
 
     name = models.CharField(max_length=20)
-    programme_attended = models.CharField(max_length=50)
+    programme_attended = models.CharField(max_length=20,choices=PROGRAMMES)
     your_experience = models.TextField(max_length=400)
     date = models.DateField(auto_now_add=True)
     rating = models.IntegerField(choices=STARS)
