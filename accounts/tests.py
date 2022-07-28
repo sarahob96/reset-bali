c046ef0f1e699fdb968445880653dfba83a07a87
sarahob96 <sarahobrien15@gmail.com>
2022-07-28 18:32:50 +0000

    add tests for the accounts views

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, SimpleTestCase, Client
 from django.urls import reverse, resolve
-from accounts.views import new_account, login_form, logout_page, password_changed
+from accounts.views import new_account, login_form, logout_page, password_changed 
+from accounts.forms import UserCreateForm
 
 # Create your tests here.
 
@@ -27,3 +28,20 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, password_changed)
 
    
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.new_account_url = reverse('register')
+        self.login_form_url = reverse('login')
+       
+
+    def test_register_form_GET(self):
+        client = Client()
+        response = client.get(self.new_account_url)
+        self.assertTemplateUsed(response, 'accounts/registration.html')
+    
+    def test_login_form_GET(self):
+        client = Client()
+        response = client.get(self.login_form_url)
+        self.assertTemplateUsed(response, 'accounts/login.html',)
