b29df3e67b67b5638b92d22e6eed3e50944f0612
sarahob96 <sarahobrien15@gmail.com>
2022-07-28 17:23:38 +0000

    add model and view tests for the programmes app

--- programmes/tests.py
+++ programmes/tests.py
@@ -1,6 +1,10 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase, Client
 from django.urls import reverse, resolve
 from programmes.views import rewindbooking, renewbooking, restartbooking, my_bookings, update_booking, update_renew_booking, update_restart_booking, delete_booking, delete_renew_booking, delete_restart_booking, ubud, seminyak
+from programmes.models import Restart, Rewind, Renew
+from programmes.forms import Rewind_form, Restart_form, Renew_form
+
+
 # Create your tests here.
 
 class TestUrls(SimpleTestCase):
@@ -64,3 +68,64 @@ class TestUrls(SimpleTestCase):
         url = reverse('ubud')
         print(resolve(url))
         self.assertEquals(resolve(url).func, ubud)
+
+
+class TestModels(TestCase):
+    
+
+    def test_rewind_empty_form(self):
+        form = Rewind_form()
+        self.assertIn("phone", form.fields)
+        self.assertIn("email", form.fields)
+    
+    def test_renew_empty_form(self):
+        form = Renew_form()
+        self.assertIn("phone", form.fields)
+        self.assertIn("email", form.fields)
+
+    def test_restart_empty_form(self):
+        form = Restart_form()
+        self.assertIn("phone", form.fields)
+        self.assertIn("email", form.fields)   
+    
+    def test_rewind_form_no_data(self):
+        form = Rewind_form(data={})
+
+        self.assertFalse(form.is_valid())
+    
+    def test_renew_form_no_data(self):
+        form = Renew_form(data={})
+
+        self.assertFalse(form.is_valid())
+       
+    def test_restart_form_no_data(self):
+        form = Restart_form(data={})
+
+        self.assertFalse(form.is_valid())
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.rewindbooking_url = reverse('rewindbooking')
+        self.renewbooking_url = reverse('renewbooking')
+        self.restartbooking_url = reverse('restartbooking')
+        self.update_booking_url = reverse('update_booking', args=['1'])
+        self.update_renew_booking_url = reverse('update_renew_booking', args=['1'])
+
+    def test_rewindbooking_GET(self):
+        client = Client()
+        response = client.get(self.rewindbooking_url)
+        self.assertTemplateUsed(response, 'programmes/rewind.html')
+
+    def test_renewbooking_GET(self):
+        client = Client()
+        response = client.get(self.renewbooking_url)
+        self.assertTemplateUsed(response, 'programmes/renew.html')
+
+    def test_restartbooking_GET(self):
+        client = Client()
+        response = client.get(self.restartbooking_url)
+        self.assertTemplateUsed(response, 'programmes/restart.html')
+
